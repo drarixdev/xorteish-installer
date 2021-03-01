89237efeac75f806466975943369f8745a175836
Drarix <69253068+drarixdev@users.noreply.github.com>
2021-03-01 17:21:10 +0100

    Create install-xorteish-prealpha-1-1.py

---
+++ install-xorteish-prealpha-1-1.py
@@ -0,0 +1,18 @@
+import requests 
+import zipfile
+import os
+archive_url = "https://github.com/drarixdev/xorteish/archive/pre-alpha1-1.zip"
+name_archive = "pre-alpha1-1.zip"
+print("Installing please wait")
+
+
+r = requests.get(archive_url) 
+
+with open(name_archive,'wb') as f: 
+  
+ 
+    f.write(r.content) 
+
+with zipfile.ZipFile(name_archive, 'r') as zip_ref:
+    zip_ref.extractall()
+os.remove(name_archive)
